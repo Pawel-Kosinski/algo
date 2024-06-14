971fdf73aec9af3b39c1051279e2ac1228e23fff
Mangner <05langner20@gmail.com>
2024-06-14 19:42:43 +0200

    Dodanie Graficznego Interfejsu Uzytkownika

--- chromosom.py
+++ chromosom.py
@@ -1,41 +1,110 @@
 import random
-
-#cały chromosom działa na zadanym przedziale (bez obliczania jeszcze odpowiedniego ciągu znaków od książka)
-#chromosom przyjmuje póki co tylko x
-#poniżej zawarte są funkcje dec_to_bin, bin_to_dec - ich nie trzeba tłumaczyć chyba
-#w klasie chromosom inicjalizacja tablicy (liczby sys dziesiętny)
-#następnie są funkcje obsługujące różne rodzaje mutacji
-#funkcja multuplication która obsługuje różne rodzaje krzyżowań, mutacji (wybór przez string) z zadanym prawdopodobieństwem - na binarnych
-#multiplication zwraca listę skrzyżowanych, zmutowanych, dziesiętnych liczb
+import math
 
 
+rangeStart = -10
+rangeEnd = 10
+decimalApproximation = 6                                                                
+bin_len = math.ceil(math.log2((rangeEnd-rangeStart) * (10 ** decimalApproximation)))       
+childPerGen = 100                                                                          
 
-#funckja dec to bin jest do dostosowania - zgodnie ze wzorem książka + konfiguracja dokładności
-def dec_to_bin(n):
-    n = round(n)
-    if not -10 <= n <= 10:
-        raise ValueError("Input should be within the interval [-10, 10]")
-    return format(n & 0b1111111111111111111111111, '025b')
 
-def bin_to_dec(b):
-    return int(b, 2)
+def set_Global_Values(start_Value : float, end_Value : float, number_Of_Approx_Bits : int, population_Amount : int) -> None:
+    "Funkcja ustawia zmienne Globalne" 
+    global rangeStart, rangeEnd, decimalApproximation, bin_len, childPerGen
+
+    rangeStart = start_Value
+    rangeEnd = end_Value
+    decimalApproximation = number_Of_Approx_Bits
+    bin_len = math.ceil(math.log2((rangeEnd - rangeStart) * (10 ** decimalApproximation)))
+    childPerGen = population_Amount
+
+
+def convert_Dec_to_Bin(dec : float) -> str:
+    "Zamiana liczby decymalnej na binarna"
+    if not rangeStart <= dec <= rangeEnd:
+        raise ValueError(f"Input should be within the interval [{rangeStart}, {rangeEnd}]")
+    
+    scaled_value = int((dec - rangeStart) / (rangeEnd - rangeStart) * (2 ** bin_len - 1))
+    return format(scaled_value, f'0{bin_len}b')
+
+
+def convert_Bin_to_Dec(bin : str) -> float:
+    "Zamienia liczbe binarną na decymalna"
+    integer = int(bin, 2)  
+    return rangeStart + integer * (rangeEnd - rangeStart) / (2 ** bin_len - 1)
+
+
 
 class Chromosome:
-    def __init__(self):
-        self.chromosome = [random.uniform(-10, 10) for _ in range(100)] #przedział [-10, 10]
+    """Klasa Chromosome zawiera metody do przeprowadzania krzyżowania i mutacji na chromosomach oraz funkcję multiplication,
+        która wykonuje zadane krzyżowanie i mutację na podanej populacji osobników."""
 
-    def edge_mutation(self, child, mutation_prob):
+    def __init__(self):
+        "Tworzy tablicę losowych wartości od rangeStart do rangeEnd."
+        self.chromosome = [random.uniform(rangeStart, rangeEnd) for _ in range(100)] 
+
+
+    def one_Point_Crossover(self, parent1 : str, parent2 : str) -> str:
+        "Przeprowadza krzyżowanie jednopunktowe między dwoma rodzicami."
+        crossover_Point = random.randint(1, len(parent1) - 1)
+        child = parent1[:crossover_Point] + parent2[crossover_Point:]
+        return child
+
+
+    def two_Point_Crossover(self, parent1 : str, parent2 : str) -> str:
+        "Przeprowadza krzyżowanie dwupunktowe między dwoma rodzicami."
+        cross_Point_One, cross_Point_Two = sorted(random.sample(range(1, len(parent1) - 1), 2)) 
+        child = parent1[:cross_Point_One] + parent2[cross_Point_One : cross_Point_Two] + parent1[cross_Point_Two:]
+        return child
+
+
+    def three_Point_Crossover(self, parent1 : str, parent2 : str) -> str:
+        "Przeprowadza krzyżowanie trzypunktowe między dwoma rodzicami."
+        cross_Point_One, cross_Point_Two, cross_Point_Three = sorted(random.sample(range(1, len(parent1) - 1), 3))
+        child = parent1[:cross_Point_One] + parent2[cross_Point_One : cross_Point_Two] + parent1[cross_Point_Two : cross_Point_Three] + parent2[cross_Point_Three:]
+        return child
+
+
+    def uniform_Crossover(self, parent1 : str, parent2 : str) -> str:
+        "Przeprowadza krzyżowanie jednorodne między dwoma rodzicami."
+        child_Bits = [parent1[i] if random.random() < 0.5 else parent2[i] for i in range(len(parent1))]
+        child = ''.join(child_Bits)
+        return child
+
+
+    def grainy_Crossover(self, parent1 : str, parent2 : str) -> str:
+        "Przeprowadza krzyżowanie ziarniste między dwoma rodzicami."
+        grain_size = random.randint(1, 5)
+        child_bits = []
+        use_parent1 = True
+        i = 0
+        while i < len(parent1):
+            end = min(i + grain_size, len(parent1))
+            child_bits.append(parent1[i:end] if use_parent1 else parent2[i:end])
+            use_parent1 = not use_parent1
+            i = end
+        child = ''.join(child_bits)
+        return child
+
+
+    def edge_Mutation(self, child : str, mutation_prob : float) -> str:
+        "Przeprowadza mutację krawędziową na potomku."
         if random.random() < mutation_prob:
             child = ('1' if child[0] == '0' else '0') + child[1:-1] + ('1' if child[-1] == '0' else '0')
         return child
 
-    def one_point_mutation(self, child, mutation_prob):
+
+    def one_Point_Mutation(self, child : str, mutation_prob : float) -> str:
+        "Przeprowadza mutację jednopunktową na potomku."
         if random.random() < mutation_prob:
             mutation_point = random.randint(0, len(child) - 1)
             child = child[:mutation_point] + ('1' if child[mutation_point] == '0' else '0') + child[mutation_point + 1:]
         return child
 
-    def two_point_mutation(self, child, mutation_prob):
+
+    def two_Point_Mutation(self, child : str, mutation_prob : float) -> str:
+        "Przeprowadza mutację dwupunktową na potomku."
         if random.random() < mutation_prob:
             mp1, mp2 = sorted(random.sample(range(len(child)), 2))
             child = (child[:mp1] + ('1' if child[mp1] == '0' else '0') +
@@ -43,43 +112,57 @@ class Chromosome:
                      child[mp2 + 1:])
         return child
 
-    def multiplication(self, parents, crossover_type="two_point", mutation_type="edge", mutation_prob=0.2):
+
+    def mutation(self, child : str, mutation_Method : str, mutation_Probability : float) -> str:
+        "Wybiera i przeprowadza odpowiednią metodę mutacji na potomku."
+        
+        if mutation_Method == "Edge Mutation":
+            child = self.edge_Mutation(child, mutation_Probability)
+
+        elif mutation_Method == "One Point Mutation":
+            child = self.one_Point_Mutation(child, mutation_Probability)
+
+        elif mutation_Method == "Two Point Mutation":
+            child = self.two_Point_Mutation(child, mutation_Probability)
+            
+        return child
+
+
+    def multiplication(self, parents : list, crossover_Method="two_point", mutation_Method="edge", mutation_Probability=0.2) -> list:
+        '''Funkcja przeprowadzająca krzyżowanie i mutację na podanej populacji.
+
+        Args:
+            parents (list): Populacja osobników do przekształcenia.
+            crossover_type (str): Typ krzyżowania. Domyślnie "two_point".
+            mutation_type (str): Typ mutacji. Domyślnie "edge".
+            mutation_prob (float): Prawdopodobieństwo mutacji. Domyślnie 0.2.
+
+        Returns:
+            list: Nowa populacja po przeprowadzeniu krzyżowania i mutacji.
+        '''
         new_generation = []
-        for _ in range(100):
+
+        for _ in range(childPerGen):
             parent1, parent2 = random.sample(parents, 2)
-            parent1, parent2 = dec_to_bin(parent1), dec_to_bin(parent2)
+            parent1, parent2 = convert_Dec_to_Bin(parent1), convert_Dec_to_Bin(parent2)
 
-            if crossover_type == "one_point":
-                crossover_point = random.randint(1, len(parent1) - 1)
-                child = parent1[:crossover_point] + parent2[crossover_point:]
-            elif crossover_type == "two_point":
-                cp1, cp2 = sorted(random.sample(range(1, len(parent1) - 1), 2))
-                child = parent1[:cp1] + parent2[cp1:cp2] + parent1[cp2:]
-            elif crossover_type == "three_point":
-                cp1, cp2, cp3 = sorted(random.sample(range(1, len(parent1) - 1), 3))
-                child = parent1[:cp1] + parent2[cp1:cp2] + parent1[cp2:cp3] + parent2[cp3:]
-            elif crossover_type == "uniform":
-                child_bits = [parent1[i] if random.random() < 0.5 else parent2[i] for i in range(len(parent1))]
-                child = ''.join(child_bits)
-            elif crossover_type == "grainy":
-                grain_size = random.randint(1, 5)
-                child_bits = []
-                use_parent1 = True
-                i = 0
-                while i < len(parent1):
-                    end = min(i + grain_size, len(parent1))
-                    child_bits.append(parent1[i:end] if use_parent1 else parent2[i:end])
-                    use_parent1 = not use_parent1
-                    i = end
-                child = ''.join(child_bits)
+            if crossover_Method == "One Point CrossOver":
+                child = self.one_Point_Crossover(parent1, parent2)
 
-            # Apply mutation
-            if mutation_type == "edge":
-                child = self.edge_mutation(child, mutation_prob)
-            elif mutation_type == "one_point":
-                child = self.one_point_mutation(child, mutation_prob)
-            elif mutation_type == "two_point":
-                child = self.two_point_mutation(child, mutation_prob)
+            elif crossover_Method == "Two Point CrossOver":
+                child = self.two_Point_Crossover(parent1, parent2)
 
-            new_generation.append(bin_to_dec(child))
+            elif crossover_Method == "Three Point CrossOver":
+                child = self.three_Point_Crossover(parent1, parent2)
+
+            elif crossover_Method == "Uniform CrossOver":
+                child = self.uniform_Crossover(parent1, parent2)
+
+            elif crossover_Method == "Grainy CrossOver":
+                child = self.grainy_Crossover(parent1, parent2)
+
+            child = self.mutation(child, mutation_Method, mutation_Probability)
+
+            new_generation.append(convert_Bin_to_Dec(child))
+
         return new_generation
---
+++ main.py
@@ -0,0 +1,9 @@
+from GUI import GUI
+
+
+def main():
+    application = GUI("Obliczenia Ewolucyjne" ,"1080x720")
+    application.run_GUI()
+
+
+main()
